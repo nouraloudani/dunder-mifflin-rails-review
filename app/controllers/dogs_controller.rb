210eef6ba88d4f79487b7bf4406cc87ad271a044
Noura <contact.nouraloudani@gmail.com>
2020-12-15 02:30:37 -0500

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -8,5 +8,21 @@ class DogsController < ApplicationController
         @dog = Dog.find(params[:id])
     end
 
+    # <% if emp.dog_id == @dog %>
+    #     <p><%= emp.first_name%></p>
+    #     <% end  %>
+
+
+# <% Employee.all.map do |emp| %>
+#     <% if emp.dog_id == @dog %> 
+#     <p><%= emp.first_name %></p>
+#     <% end %>
+
+# <% end %>
+
+# <% Employee.all.map do |emp|%>
+#     <p><%= emp.dog_id %><p>
+#     <p><%= @dog.id %><p>
+# <% end  %>
 
 end
